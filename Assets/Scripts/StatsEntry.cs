7af01428efc308ec8b37a8fc4cd4d572ea333eaf
Leo Ji <sj3026@nyu.edu>
2022-03-26 23:42:58 -0400

    added buttons for stats entries

---
+++ Assets/Scripts/StatsEntry.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/*this class control and store a int value of a stat
+  it allows player using button to change value*/
+public class StatsEntry : MonoBehaviour
+{
+    int statValue = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
